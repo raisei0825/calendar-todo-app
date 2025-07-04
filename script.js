8da572be1ec871e4cd8d968c5aed0d859bc8887d
raisei0825 <raise0825@gmail.com>
2025-07-04 22:54:21 +0900

    基盤のカレンダー完成

--- script.js
+++ script.js
@@ -1,39 +1,60 @@
 const weeks = ["日", "月", "火", "水", "木", "金", "土"];
-const date = new Date();
-const year = date.getFullYear();
-const month = date.getMonth() + 1;
-const startDate = new Date(year, month - 1, 1); // 月の最初の日を取得
-const endDate = new Date(year, month, 0); // 月の最後の日を取得
-const endDayCount = endDate.getDate(); // 月の末日
-const startDay = startDate.getDay(); // 月の最初の日の曜日を取得
-let dayCount = 1; // 日にちのカウント
-let calendarHtml = ""; // HTMLを組み立てる変数
 
-calendarHtml += "<h1>" + year + "/" + month + "</h1>";
+// 初期化
+const date = new Date();
+// 年取得
+const year = date.getFullYear();
+// 月取得
+const month = date.getMonth() + 1;
+// 日にち取得
+const today = date.getDate();
+
+//月の最初の日を取得
+// 例：`year = 2025`, `month = 7`（7月）のとき → `new Date(2025, 6, 1)` → 2025年7月1日
+const startDate = new Date(year, month - 1, 1);
+const startDay = startDate.getDay();
+
+// 月の最後の日を取得
+// month 月の 0日 は、JavaScriptでは 前月の末日 を意味します。
+// でも Date(year, 7, 0) にすると、JavaScriptは 「7月0日」= 6月30日 と解釈する
+// つまり 「7月の0日目＝6月の最終日」この「テクニック」で、1か月先を指定し、その0日目を取ると、前の月の末日が取れる！
+const endDate = new Date(year, month, 0);
+
+// `endDate` から「日（day）」の数値を取り出す。
+// 例：7月なら → `endDay = 31`
+const endDay = endDate.getDate();
+
+let dayCount = 1;
+// カレンダー作成
+let calendarHtml = "";
+
+calendarHtml += `<h1>${year}年${month}月${today}日</h1>`;
 calendarHtml += "<table>";
 
-// 曜日の行を作成
 for (let i = 0; i < weeks.length; i++) {
-  calendarHtml += "<td>" + weeks[i] + "</td>";
+  calendarHtml += `<td>${weeks[i]}</td>`;
 }
-
-for (let w = 0; w < 6; w++) {
-  calendarHtml += "<tr>";
-
+for (let j = 0; j < 6; j++) {
+  // 縦の配列を作成
+  calendarHtml += `<tr>`;
   for (let d = 0; d < 7; d++) {
-    if (w == 0 && d < startDay) {
-      // 1行目で1日の曜日の前
-      calendarHtml += "<td></td>";
-    } else if (dayCount > endDayCount) {
-      // 末尾の日数を超えた
-      calendarHtml += "<td></td>";
+    if (j == 0 && d < startDay) {
+      // 最初の行（1週目）で、月の始まりが水曜（startDay = 3）などの場合、その前に空白セルを入れる。
+      calendarHtml += `<td></td>`;
+    } else if (dayCount > endDay) {
+      // 月末を過ぎたら、残りのセルも空白にする。
+      calendarHtml += `<td></td>`;
+    } else if (dayCount === today) {
+      calendarHtml += "<td class='today'>" + dayCount + "</td>";
+      dayCount++;
     } else {
       calendarHtml += "<td>" + dayCount + "</td>";
       dayCount++;
     }
   }
-  calendarHtml += "</tr>";
+  calendarHtml += `</tr>`;
 }
+
 calendarHtml += "</table>";
 
 document.querySelector("#calendar").innerHTML = calendarHtml;
